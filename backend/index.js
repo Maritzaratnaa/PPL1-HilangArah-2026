534ce0af7812148337dc27f3d67f0c2d8d1bfe12
Audrey Shaina Tjandra <audrey1409.ast@gmail.com>
2026-03-13 20:27:48 +0700

    feat: api profile

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
+const profileRoutes = require('./src/routes/profileRoutes')
 
 const app = express();
 app.use(express.json()); 
 
 app.use('/api', authRoutes);
+app.unsubscribe('/api/profile', profileRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header('Authorization')?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({message: "Akses ditolak, token tidak ada."});   
+    }
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    }
+    catch (error) {
+        res.status(400).json({message: "Token tidak valid!"});
+    }
+};
