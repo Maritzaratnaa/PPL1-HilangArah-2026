e99e6ffeeb89fbba7b0f791d1bb33e527fe8455f
Audrey Shaina Tjandra <audrey1409.ast@gmail.com>
2026-03-15 16:33:12 +0700

    api edit profile

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/profileController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/', authMiddleware, authController.getProfile);
