aa4862aba96309c3802fb085a1bf4752db805915
Audrey Shaina Tjandra <audrey1409.ast@gmail.com>
2026-03-28 23:16:06 +0700

    fix database

--- database/init.sql
+++ database/init.sql
@@ -15,7 +15,8 @@ CREATE TABLE profiles (
     full_name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20),
     category_status ENUM('Disabilitas', 'Lansia', 'Ibu Hamil', 'Penyakit Rentan', 'Anak', 'Perempuan'),
-    font_size_pref ENUM ('Small', 'Medium', 'Large') DEFAULT 'Medium'
+    font_size_pref ENUM ('Small', 'Medium', 'Large') DEFAULT 'Medium',
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE guides (
